78c7ef63f2bb06f72d3252cb63edf208d1b3c326
ceduardooliveira <c-eduardo94@hotmail.com>
2025-03-26 11:41:46 -0300

    draw small card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BigCardComponent } from "../../components/big-card/big-card.component";
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
+import { SmallCardComponent } from "../../components/small-card/small-card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [BigCardComponent, MenuTitleComponent],
+  imports: [BigCardComponent, MenuTitleComponent, SmallCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
